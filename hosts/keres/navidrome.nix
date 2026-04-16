f50ff524d9252883205f9e52226d31ac7c458bc9
jamie <git@skilet.ro>
2026-04-16 17:02:42 +0100

    feat(navidrome): update settings

--- hosts/keres/navidrome.nix
+++ hosts/keres/navidrome.nix
@@ -1,6 +1,7 @@
-{ self', ... }:
+{ self', lib, ... }:
 {
   nixos =
+    { pkgs, ... }:
     let
       subdomain = "navidrome";
       domain = "warm.vodka";
@@ -14,8 +15,23 @@
           Port = port;
           MusicFolder = "/mnt/music";
           EnableSharing = true;
+          UIWelcomeMessage = "Hi! Please use a dedicated Desktop/Mobile app instead of this frontend.";
+          Plugins.Enabled = true;
+          Agents = "deezer,audiomuseai";
         };
       };
+
+      systemd.services.navidrome.serviceConfig.ExecStartPre =
+        let
+          audiomuseai = pkgs.fetchurl {
+            url = "https://github.com/NeptuneHub/AudioMuse-AI-NV-plugin/releases/download/v7/audiomuseai.ndp";
+            sha256 = "1y23vb5kip1gzd0i1c7lr4dwamyn8irzql5y0zhz17gbqf1w5dzs";
+          };
+        in
+        [
+          "${lib.getExe' pkgs.coreutils "mkdir"} -p /var/lib/navidrome/plugins"
+          "${lib.getExe' pkgs.coreutils "cp"} -f ${audiomuseai} /var/lib/navidrome/plugins/audiomuseai.ndp"
+        ];
 
       systemd.tmpfiles.rules = [
         "d /mnt/music 0770 navidrome navidrome -"
