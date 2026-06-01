40fc5d9e8583d25e114793040bfbaa5ab7703c23
jamie <git@skilet.ro>
2026-06-01 19:45:22 +0100

    refactor(wakatime): move module logic into bundle

--- bundles/programs/wakatime.nix
+++ bundles/programs/wakatime.nix
@@ -1,16 +1,24 @@
-{ bundleLib, ... }:
+{ bundleLib, inputs', ... }:
 bundleLib.mkEnableModule [ "gaia" "programs" "wakatime" ] {
 
   home-manager =
     { pkgs, config, ... }:
     {
-      programs'.wakatime = {
-        enable = true;
-        settings = {
-          api_url = "https://wakapi.warm.vodka/api";
-          api_key_vault_cmd = "${pkgs.writeShellScript "cat-wakatime-api-key" "cat ${config.sops.secrets.wakapi-key.path}"}";
-        };
-      };
+      home.packages = [
+        pkgs.wakatime-cli
+        inputs'.wakatime-ls.packages.default
+      ];
+
+      home.sessionVariables.WAKATIME_HOME = "${config.xdg.configHome}/wakatime";
+
+      xdg.configFile."wakatime/.wakatime.cfg".source =
+        (pkgs.formats.ini { }).generate "gaia-wakatime-cfg"
+          {
+            settings = {
+              api_url = "https://wakapi.warm.vodka/api";
+              api_key_vault_cmd = "${pkgs.writeShellScript "cat-wakatime-api-key" "cat ${config.sops.secrets.wakapi-key.path}"}";
+            };
+          };
 
       sops.secrets."wakapi-key" = { };
     };
