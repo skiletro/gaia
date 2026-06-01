08a4983599873a1e7209fe32535c9a3af5f5243f
jamie <git@skilet.ro>
2026-06-01 19:45:32 +0100

    refactor(gsr): move module logic into bundle

--- bundles/programs/gsr.nix
+++ bundles/programs/gsr.nix
@@ -1,7 +1,30 @@
-{ bundleLib, ... }:
+{ bundleLib, lib, ... }:
 bundleLib.mkEnableModule [ "gaia" "programs" "gsr" ] {
 
-  nixos.programs'.gpu-screen-recorder-ui.enable = true;
+  nixos =
+    { self', config, ... }:
+    let
+      package = self'.packages.gpu-screen-recorder-ui.override {
+        inherit (config.security) wrapperDir;
+      };
+    in
+    {
+      programs.gpu-screen-recorder.enable = lib.mkDefault true;
+
+      environment.systemPackages = [ package ];
+
+      systemd.packages = [ package ];
+
+      security.wrappers."gsr-global-hotkeys" = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_setuid+ep";
+        source = lib.getExe' package "gsr-global-hotkeys";
+      };
+
+      systemd.user.services.gpu-screen-recorder-ui.wantedBy = [ "default.target" ]; # Start on startup
+
+    };
 
   darwin = throw "gaia: 'gsr' is incompatible with macos.";
 
