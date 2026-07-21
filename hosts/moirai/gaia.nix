29dc8923cfc075d221ed21771f15e366a53baff2
jamie <git@skilet.ro>
2026-07-21 11:57:50 +0100

    feat(moirai): update gaia

--- hosts/moirai/gaia.nix
+++ hosts/moirai/gaia.nix
@@ -2,24 +2,31 @@
   gaia = {
     desktop = "niri";
     system = {
+      # keep-sorted start
       bluetooth.enable = true;
       fonts.enable = true;
       greeter.enable = true;
+      # keep-sorted end
     };
     services = {
+      # keep-sorted start
       flatpak.enable = true;
       printing.enable = true;
       wireguard.enable = true;
+      # keep-sorted end
     };
     programs = {
+      # keep-sorted start
       btop.enable = true;
       devenv.enable = true;
       git.enable = true;
       helium.enable = true;
       helix.enable = true;
       nu.enable = true;
+      signal.enable = true;
       term-utils.enable = true;
       zoxide.enable = true;
+      # keep-sorted end
     };
     state.system = "26.11";
   };
