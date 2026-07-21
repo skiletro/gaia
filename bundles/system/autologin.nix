b1c335b8ffa96c04e627d0c2bd7c6a61a5c37eda
jamie <git@skilet.ro>
2026-07-21 21:25:20 +0100

    feat(autoboot): add autoboot bundle

---
+++ bundles/system/autologin.nix
@@ -0,0 +1,27 @@
+{ bundleLib, lib, ... }:
+bundleLib.mkEnableModule [ "gaia" "system" "autologin" ] {
+
+  nixos =
+    { pkgs, ... }:
+    {
+
+      services.greetd.settings.initial_session = {
+        command = "uwsm start -- niri-uwsm.desktop";
+        user = "jamie";
+      };
+
+      systemd.services.greetd.serviceConfig = {
+        KeyringMode = lib.mkForce "inherit";
+        Type = lib.mkForce "idle";
+      };
+
+      security.pam.services.greetd.rules.session.fde-boot-pw = {
+        control = "optional";
+        modulePath = "${pkgs.pam_fde_boot_pw}/lib/security/pam_fde_boot_pw.so";
+        settings.inject_for = "gkr";
+        order = 12500;
+      };
+
+    };
+
+}
