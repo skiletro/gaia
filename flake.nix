3d14d0f6f400c91a7183157f05acd55f1e6b82e1
jamie <git@skilet.ro>
2026-07-22 12:25:36 +0100

    feat(flake): remove redundant substituters and neaten up

--- flake.nix
+++ flake.nix
@@ -116,22 +116,24 @@
 
   nixConfig = {
     extra-trusted-substituters = [
-      "https://nix-community.cachix.org"
+      # keep-sorted start
+      "https://helix.cachix.org"
       "https://hyprland.cachix.org"
       "https://niri.cachix.org"
-      "https://attic.xuyh0120.win/lantian"
-      "https://helix.cachix.org"
-      "https://noctalia.cachix.org"
+      "https://nix-community.cachix.org"
       "https://nixos-apple-silicon.cachix.org"
+      "https://noctalia.cachix.org"
+      # keep-sorted end
     ];
     extra-trusted-public-keys = [
+      # keep-sorted start
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      # keep-sorted end
     ];
   };
 }
