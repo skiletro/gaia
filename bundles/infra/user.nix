eb94039b33b80e4eca824b90de7d8793abf7e141
jamie <git@skilet.ro>
2026-07-23 23:05:35 +0100

    feat(user): try to declutter home dir

--- bundles/infra/user.nix
+++ bundles/infra/user.nix
@@ -37,6 +37,36 @@ in
         inherit username;
         homeDirectory = "/home/${username}";
         preferXdgDirectories = true;
+        # Try and clean up our home/ directory as much as possible.
+        sessionVariables = with config.xdg; {
+          # keep-sorted start
+          ANDROID_AVD_HOME = "${dataHome}/android/avd";
+          ANDROID_USER_HOME = "${dataHome}/android";
+          BUN_INSTALL_GLOBAL_DIR = "${dataHome}/bun";
+          CARGO_HOME = "${dataHome}/cargo";
+          DOTNET_CLI_HOME = "${dataHome}/dotnet";
+          GOPATH = "${dataHome}/go";
+          GRADLE_USER_HOME = "${dataHome}/gradle";
+          GTK2_RC_FILES = "${configHome}/gtk-2.0/gtkrc";
+          HISTFILE = "${stateHome}/bash/history";
+          LESSHISTFILE = "${cacheHome}/less/history";
+          NODE_REPL_HISTORY = "${stateHome}/node_repl_history";
+          NPM_CONFIG_CACHE = "${cacheHome}/npm";
+          NPM_CONFIG_INIT_MODULE = "${configHome}/npm/config/npm-init.js";
+          NPM_CONFIG_PREFIX = "${stateHome}/npm";
+          NPM_CONFIG_TMP = "${stateHome}/npm";
+          NUGET_PACKAGES = "${cacheHome}/NuGetPackages";
+          OMNISHARPHOME = "${configHome}/omnisharp";
+          RUSTUP_HOME = "${dataHome}/rustup";
+          STACK_ROOT = "${dataHome}/stack";
+          TERMINFO = "${dataHome}/terminfo";
+          TERMINFO_DIRS = "${dataHome}/terminfo\${TERMINFO_DIRS:+:$TERMINFO_DIRS}";
+          VAGRANT_HOME = "${dataHome}/vagrant";
+          WINEPREFIX = "${dataHome}/wine";
+          XCOMPOSECACHE = "${cacheHome}/X11/xcompose";
+          _Z_DATA = "${dataHome}/z";
+          # keep-sorted end
+        };
       };
 
       xresources.path = "${config.xdg.configHome}/.Xresources";
