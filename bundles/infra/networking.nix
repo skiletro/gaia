e3180812e833461fa9817e844bff4e380ce551cb
jamie <git@skilet.ro>
2026-08-04 11:04:50 +0100

    feat: don't wait until nm is online before booting

--- bundles/infra/networking.nix
+++ bundles/infra/networking.nix
@@ -2,6 +2,8 @@
   nixos = {
     networking.networkmanager.enable = true;
 
+    systemd.services.NetworkManager-wait-online.enable = false;
+
     users.users.jamie.extraGroups = [ "networkmanager" ];
   };
 }
