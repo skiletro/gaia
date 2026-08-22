243c0b00230282f18e6b66656e628a50fed6dacb
jamie <git@skilet.ro>
2026-08-22 20:05:04 +0100

    fix(proton): remove gui; tui only

--- bundles/programs/proton.nix
+++ bundles/programs/proton.nix
@@ -2,10 +2,7 @@
 bundleLib.mkEnableModule ["gaia" "programs" "proton"] {
   home-manager = {pkgs, ...}: {
     home.packages = with pkgs; [
-      protonmail-desktop
-      proton-pass
       proton-pass-cli
-      proton-vpn
       proton-vpn-cli
     ];
   };
