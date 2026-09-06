c494093115c8591b6120048e4425b02e5da3a534
jamie <git@skilet.ro>
2026-09-06 12:23:35 +0100

    chore: use upstream version of gsr-ui, remove flake package

--- bundles/programs/gsr.nix
+++ bundles/programs/gsr.nix
@@ -1,29 +1,11 @@
-{
-  bundleLib,
-  lib,
-  ...
-}:
+{bundleLib, ...}:
 bundleLib.mkEnableModule ["gaia" "programs" "gsr"] {
-  gaia.autoStart = ["gsr-ui launch-hide-announce"];
+  gaia.autoStart = ["gsr-ui launch-daemon"];
 
   nixos = {
-    self',
-    config,
-    ...
-  }: let
-    package = self'.packages.gpu-screen-recorder-ui.override {
-      inherit (config.security) wrapperDir;
-    };
-  in {
-    programs.gpu-screen-recorder.enable = lib.mkDefault true;
-
-    environment.systemPackages = [package];
-
-    security.wrappers."gsr-global-hotkeys" = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_setuid+ep";
-      source = lib.getExe' package "gsr-global-hotkeys";
+    programs.gpu-screen-recorder = {
+      enable = true;
+      ui.enable = true;
     };
   };
 }
