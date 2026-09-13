7739e4d4115690ee5d6ee90f72967d21e90cae04
jamie <git@skilet.ro>
2026-09-13 16:18:29 +0100

    feat(navidrome): update beets config

--- hosts/keres/navidrome.nix
+++ hosts/keres/navidrome.nix
@@ -1,20 +1,21 @@
-{
-  nixos = {pkgs, ...}: let
-    subdomain = "navidrome";
-    domain = "warm.vodka";
-    port = 4533;
+let
+  subdomain = "navidrome";
+  domain = "warm.vodka";
+  port = 4533;
 
-    rootFolder = "/srv/navidrome";
-  in {
+  RootFolder = "/srv/navidrome";
+  MusicFolder = "${RootFolder}/music";
+  DataFolder = "${RootFolder}/data";
+  CacheFolder = "${RootFolder}/cache";
+in {
+  nixos = {
     services.navidrome = {
       enable = true;
       settings = {
         Address = "127.0.0.1";
         Port = port;
-        MusicFolder = "${rootFolder}/music";
-        DataFolder = "${rootFolder}/data";
-        CacheFolder = "${rootFolder}/cache";
         DefaultTheme = "Spotify-ish";
+        inherit MusicFolder DataFolder CacheFolder;
         EnableSharing = false;
         EnableStarRating = false;
       };
@@ -24,10 +25,54 @@
       reverse_proxy :${toString port}
     '';
 
-    environment.systemPackages = [
-      pkgs.beets
-    ];
-
     users.users.jamie.extraGroups = ["navidrome"];
+  };
+
+  home-manager = {
+    programs.beets = {
+      enable = true;
+      settings = {
+        library = MusicFolder;
+
+        plugins = [
+          # keep-sorted start
+          "badfiles"
+          "duplicates"
+          "fetchart"
+          "lastgenre"
+          "lyrics"
+          "musicbrainz"
+          "spotify"
+          "zero"
+          # keep-sorted end
+        ];
+
+        fetchart = {
+          sources = "coverart itunes amazon albumart filesystem";
+          cautious = true;
+          store_source = true;
+          minwidth = 1200;
+          maxwidth = 1200;
+        };
+
+        lyrics = {
+          # synced works incorrectly, so the order is important (lrclib is the only one that offers synced lyrics)
+          sources = [
+            "lrclib"
+            "genius"
+            "tekstowo"
+            "google"
+          ];
+          synced = true;
+          force = true;
+        };
+
+        spotify = {
+          show_failures = true;
+        };
+
+        zero.fields = "comments";
+      };
+    };
   };
 }
