54b69bcd51eb448059c7762d66e35bea9e0687fd
jamie <git@skilet.ro>
2026-09-16 18:35:09 +0100

    fix(greetd): default to the uwsm session

--- bundles/system/autologin.nix
+++ bundles/system/autologin.nix
@@ -1,28 +1,31 @@
 {
   bundleLib,
   lib,
+  config,
   ...
-}:
-bundleLib.mkEnableModule ["gaia" "system" "autologin"] {
-  nixos = {
-    config,
-    pkgs,
-    ...
-  }: {
-    services.greetd.settings.initial_session = {
-      command = "uwsm start -- niri-uwsm.desktop";
-      user = "jamie";
-    };
+}: let
+  desktop = config.gaia.desktop;
+in
+  bundleLib.mkEnableModule ["gaia" "system" "autologin"] {
+    nixos = {
+      config,
+      pkgs,
+      ...
+    }: {
+      services.greetd.settings.initial_session = {
+        command = "${lib.getExe config.programs.uwsm.package} start -- ${desktop}-uwsm.desktop";
+        user = "jamie";
+      };
 
-    systemd.services.greetd.serviceConfig = {
-      KeyringMode = lib.mkForce "inherit";
-    };
+      systemd.services.greetd.serviceConfig = {
+        KeyringMode = lib.mkForce "inherit";
+      };
 
-    security.pam.services.login.rules.session.fde-boot-pw = {
-      control = "optional";
-      modulePath = "${pkgs.pam_fde_boot_pw}/lib/security/pam_fde_boot_pw.so";
-      settings.inject_for = "gkr";
-      order = config.security.pam.services.login.rules.session.gnome_keyring.order - 100;
+      security.pam.services.login.rules.session.fde-boot-pw = {
+        control = "optional";
+        modulePath = "${pkgs.pam_fde_boot_pw}/lib/security/pam_fde_boot_pw.so";
+        settings.inject_for = "gkr";
+        order = config.security.pam.services.login.rules.session.gnome_keyring.order - 100;
+      };
     };
-  };
-}
+  }
